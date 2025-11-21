125ff4fba4f3d67cfe21a6090765b3c0dfcf585a
Jonathan <Jonathandaquis@gmail.com>
2025-11-21 13:35:45 +0400

    Assessment1

---
+++ Exercise 01 - Coding is Cool/Coding Is Cool Exercise.py
@@ -0,0 +1,8 @@
+#Place Values on Variable
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+
+# Use string concatenation to combine the variables and print the phrase
+print (word1,word2,word3) #Prints Word1,Word2 and Word3
+
